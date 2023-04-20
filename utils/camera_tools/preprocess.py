aeec0bdb91cd1378724e4216b8db58b011bd455a
wyvern <60736592+kelco-chan@users.noreply.github.com>
2023-04-21 05:35:35 +1000

    feat(preprocess): fixed value mask

--- utils/camera_tools/preprocess.py
+++ utils/camera_tools/preprocess.py
@@ -1,28 +1,17 @@
 import cv2
 import numpy as np
 
-element = cv2.getStructuringElement(cv2.MORPH_RECT, (17,17), (8,8))
-# only focus bottom half of the screen
 def preprocess(image):
-    print(image.shape)
-    res = image
-    res = image[round(image.shape[0] / 2):image.shape[0], 0:image.shape[1]]
-
-    hsv = cv2.cvtColor(res, cv2.COLOR_BGR2HSV)
+    bgr = image[round(image.shape[0] / 2):image.shape[0], 0:image.shape[1]]
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
     hsv[..., 1] = 255
-    hsv = cv2.medianBlur(hsv, 11)
-    
+    hsv = cv2.medianBlur(hsv, 15)
     avg = np.average(hsv,(0,1))
-
     mask1 = cv2.bitwise_not(cv2.inRange(hsv, (avg[0] - 20, 0, avg[2]-20), (avg[0] + 20, 255, avg[2] + 20)))
-
-    mask2 = cv2.inRange(hsv, (0, 0, 150), (255,255,250))
-
-
+    mask2 = cv2.inRange(hsv, (0, 0, 200), (255,255,256))
     hsv = cv2.bitwise_and(hsv,hsv, mask=cv2.bitwise_and(mask1,mask2))
-
-    res = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    return res
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bgr
 
 if __name__ == "__main__":
     print('hi')
