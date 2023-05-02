34c4f91bfb3799bb8f0fd908b8a78132f555d8e1
AwesomeGuy000 <64011811+AwesomeGuy000@users.noreply.github.com>
2023-05-02 17:25:12 +1000

    updated testcam

--- w_testcam.py
+++ w_testcam.py
@@ -1,25 +1,61 @@
-# import the opencv library
-import cv2
-import time
-  
-#zipline = "nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=(int)3264, height=(int)2464, framerate=(fraction)30/1, format=(string)NV12' ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink drop=1"
-zipline = "v4l2src ! video/x-raw, format=(string)YUY2,width=640, height=480 ! videoconvert ! video/x-raw,format=BGR ! appsink"
+# MIT License
+# Copyright (c) 2019-2022 JetsonHacks
 
-# define a video capture object
-#vid = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM),format=NV12,width=640,height=480,framerate=30/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER)
-vid = cv2.VideoCapture(zipline)
-if not vid.isOpened():
-    print("l bozo")
-    exit(-1)
-while True:
-# Capture the video frame
-# by frame
-    print(vid.isOpened())
-    ret, frame = vid.read()
-    if(ret == False):
-        print("L bozo")
-        print(frame)
+# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
+# NVIDIA Jetson Nano Developer Kit using OpenCV
+# Drivers for the camera and OpenCV are included in the base image
+
+import time
+import cv2
+
+""" 
+gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+Flip the image by setting the flip_method (most common values: 0 and 2)
+display_width and display_height determine the size of each camera pane in the window on the screen
+Default 1920x1080 displayd in a 1/4 size window
+"""
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+def show_camera():
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    pipeline = gstreamer_pipeline(flip_method=2)
+    print(pipeline)
+    video_capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        ret_val, frame = video_capture.read()
+        cv2.imwrite("camera.test.png", frame)
+        print("qutting")
+        video_capture.release()
+        cv2.destroyAllWindows()
     else:
-        cv2.imwrite('img.test.png', frame)
-        
-    time.sleep(1)
+        print("Error: Unable to open camera")
+
+if __name__ == "__main__":
+    show_camera()
