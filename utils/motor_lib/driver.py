88073420823856b18176cf93ad555d1bd1399550
AwesomeGuy000 <64011811+AwesomeGuy000@users.noreply.github.com>
2023-06-20 20:24:23 +1000

    modified driver to do stuff with arduino firmata thingy

--- utils/motor_lib/driver.py
+++ utils/motor_lib/driver.py
@@ -1,37 +1,41 @@
-from board import SCL, SDA
-import busio
-from adafruit_pca9685 import PCA9685
+# from board import SCL, SDA
+# import busio
+# from adafruit_pca9685 import PCA9685
 from time import sleep
-#from math import abs
+import math
+from pyfirmata import Arduino, util
 
 PWM_FREQ = 1000      # (Hz) max is 1.5 kHz
-MAP_CONST = 65535 / 120   # 65535 / 120 to limit speed below 100% duty cycle
+MAP_CONST = 1 / 120   # 1 / 120 to limit speed below 100% duty cycle
 HALF_WIDTH = 0.1          # Half of the width of droid, in metres
-MAX_CENT_ACC = 30000 # Maximum "centripetal acceleration" the robot is allowed to undergo. UNITS ARE DODGY, MUST BE DETERMIEND BY EXPERIMENTATION
-MAX_SPEED = MAP_CONST * 100 / 65535  # (percent) max speed of motors
+MAX_CENT_ACC = 30000   # Maximum "centripetal acceleration" the robot is allowed to undergo. UNITS ARE DODGY, MUST BE DETERMIEND BY EXPERIMENTATION
+MAX_SPEED = MAP_CONST * 100  # (percent) max speed of motors
+SERIAL_PORT = '/dev/ttyACM0' # Serial port for Arduino
 
-i2c_bus = busio.I2C(SCL, SDA)
+# Left
+#     A -> 13  
+#     B -> 12
+#   PWM -> 11
 
-# create PCA9685 class instance.
-pca = PCA9685(i2c_bus)
-pca.frequency = PWM_FREQ
+# Right
+#     A -> 8
+#     B -> 9
+#   PWM -> 10
 
-# duty_cycle is 16 bits to match other PWM objects
-# but the PCA9685 will only actually give 12 bits of resolution.
-# 65535 is the maximum value.
+# I_Sense -> 7
 
-pca.channels[0].duty_cycle = 0
-pca.channels[1].duty_cycle = 0
-pca.channels[2].duty_cycle = 0
-pca.channels[3].duty_cycle = 0
+board = Arduino(SERIAL_PORT)
+print("Communication Successfully started")
 
-motorLA = pca.channels[3]
-motorLB = pca.channels[2]
-motorRA = pca.channels[1]
-motorRB = pca.channels[0]
+motorLA = board.get_pin('d:13:o')
+motorLB = board.get_pin('d:12:o')
+motorRA = board.get_pin('d:8:o')
+motorRB = board.get_pin('d:9:o')
 
-motorENL = pca.channels[4]
-motorENR = pca.channels[5]
+motorPWML = board.get_pin('d:11:p')
+motorPWMR = board.get_pin('d:10:p')
+
+Isense = board.get_pin('d:7:i')
 
 print("Motor driver initialized. \n path: utils\motor_lib\driver.py \n PWM frequency: " + str(PWM_FREQ) + "Hz \n Max speed: " + str(MAX_SPEED) + "%")
 
@@ -39,13 +43,13 @@ print("Motor driver initialized. \n path: utils\motor_lib\driver.py \n PWM frequ
 # off/coast/stop are the same
 def off():
     # Enable pins are low during off() to coast
-    motorENL.duty_cycle = 0
-    motorENR.duty_cycle = 0
+    motorPWML.write(0)
+    motorPWMR.write(0)
 
-    motorLA.duty_cycle = 0
-    motorLB.duty_cycle = 0
-    motorRA.duty_cycle = 0
-    motorRB.duty_cycle = 0
+    motorLA.write(0)
+    motorLB.write(0)
+    motorRA.write(0)
+    motorRB.write(0)
 
 def stop():
     off()
@@ -55,43 +59,47 @@ def coast():
 
 def brake():
     off()
-    motorLA.duty_cycle = 0
-    motorLB.duty_cycle = 0
-    motorRA.duty_cycle = 0
-    motorRB.duty_cycle = 0
+    motorLA.write(0)
+    motorLB.write(0)
+    motorRA.write(0)
+    motorRB.write(0)
     # Enable pins are high during brake() to brake
-    motorENL.duty_cycle = 65535
-    motorENR.duty_cycle = 65535
+    motorPWML.write(1)
+    motorPWMR.write(1)
 
 # brakes after 1.5s of coasting
 def ebrake():
     off()
     sleep(1.5) # sleep(1.5)
-    motorLA.duty_cycle = 65535
-    motorLB.duty_cycle = 65535
-    motorRA.duty_cycle = 65535
-    motorRB.duty_cycle = 65535
+    motorLA.write(1)
+    motorLB.write(1)
+    motorRA.write(1)
+    motorRB.write(1)
 
 # forward function
 def fwd(speed, timeout=0):
-    motorLA.duty_cycle = int(speed * MAP_CONST)
-    motorLB.duty_cycle = 0
-    motorRA.duty_cycle = int(speed * MAP_CONST)
-    motorRB.duty_cycle = 0
-    motorENL.duty_cycle = 65535
-    motorENR.duty_cycle = 65535
+    motorPWML.write(speed * MAP_CONST)
+    motorPWMR.write(speed * MAP_CONST)
+
+    motorLA.write(1)
+    motorLB.write(0)
+    motorRA.write(1)
+    motorRB.write(0)
+
     if timeout > 0:
         sleep(timeout / 1000)
         off()
 
 # reverse function
 def rev(speed, timeout=0):
-    motorLA.duty_cycle = 0
-    motorLB.duty_cycle = int(speed * MAP_CONST)
-    motorRA.duty_cycle = 0
-    motorRB.duty_cycle = int(speed * MAP_CONST)
-    motorENL.duty_cycle = 65535
-    motorENR.duty_cycle = 65535
+    motorPWML.write(speed * MAP_CONST)
+    motorPWMR.write(speed * MAP_CONST)
+
+    motorLA.write(0)
+    motorLB.write(1)
+    motorRA.write(0)
+    motorRB.write(1)
+
     if timeout > 0:
         sleep(timeout / 1000)
         off()
@@ -114,8 +122,8 @@ def turn(speed: float, radius: float, timeout=0):
 def move(LIN, RIN, timeout=0):
     LIN = round(LIN / 5) * 5
     RIN = round(RIN / 5) * 5
-    L = int(LIN * MAP_CONST)  # map values to 0-65535
-    R = int(RIN * MAP_CONST)
+    L = LIN * MAP_CONST  # map values to 0-1
+    R = RIN * MAP_CONST
     #print(L, R)
     if L == 0 and R == 0:
         off()
@@ -123,28 +131,29 @@ def move(LIN, RIN, timeout=0):
     else:
         #print(L, R)
         if L > 0:
-            motorLA.duty_cycle = L
-            motorLB.duty_cycle = 0
+            motorLA.write(1)
+            motorLB.write(0)
         else:
-            motorLA.duty_cycle = 0
-            motorLB.duty_cycle = -L
+            motorLA.write(0)
+            motorLB.write(1)
         if R > 0:
-            motorRA.duty_cycle = R
-            motorRB.duty_cycle = 0
+            motorRA.write(1)
+            motorRB.write(0)
         else:
-            motorRA.duty_cycle = 0
-            motorRB.duty_cycle = -R
-    
-    motorENL.duty_cycle = 65535
-    motorENR.duty_cycle = 65535
+            motorRA.write(0)
+            motorRB.write(1)
+        
+        motorPWML.write(abs(L))
+        motorPWMR.write(abs(R))
+
     if timeout > 0:
         sleep(timeout / 1000)
         off()
 
 # Drive pins other than motor pins
-def drivePin(pin, val):
-    if pin == 0 or pin == 1 or pin == 2 or pin == 3:
-        raise Exception(f"Pin {pin} is used for motors.")
-    else:
-        pca.channels[pin].duty_cycle = int(val / 100 * 65535)
-        print(f"Pin {pin} set to {val}%")
+# def drivePin(pin, val):
+#     if pin == 0 or pin == 1 or pin == 2 or pin == 3:
+#         raise Exception(f"Pin {pin} is used for motors.")
+#     else:
+#         pca.channels[pin].duty_cycle = int(val / 100 * 65535)
+#         print(f"Pin {pin} set to {val}%")
